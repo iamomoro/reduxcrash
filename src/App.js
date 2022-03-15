7aff98ea051b9b149d0408a9a3725795e3d3b8ca
iamomoro <iamomoroit@gmail.com>
2022-03-15 15:44:58 +0300

    onChange-issue

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+
 import Posts from './components/posts';
 import Postform from './components/postform';
+import { createStore, applyMiddleware } from 'redux';
+
+const store = createStore(() => [], {}, applyMiddleware());
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <Postform />
-      <hr />
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header>
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <Postform />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
--- src/components/postform.js
+++ src/components/postform.js
@@ -1,58 +1,42 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { createPost } from '../actions/postActions';
 
-class PostForm extends Component {
+class Postform extends Component {
   constructor(props) {
     super(props);
     this.state = {
       title: '',
-      body: ''
+      body: '',
     };
 
-    this.onChange = this.onChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
+    // this.onChange = this.onChange.bind(this);
+    // this.onSubmit = this.onSubmit.bind(this);
   }
 
-  onChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-
-  onSubmit(e) {
-    e.preventDefault();
-
-    const post = {
-      title: this.state.title,
-      body: this.state.body
-    };
-
-    this.props.createPost(post);
-  }
+//   onChange(e) {
+//     this.setState({ [e.target.name]: e.target.value });
+//   }
 
   render() {
     return (
       <div>
         <h1>Add Post</h1>
-        <form onSubmit={this.onSubmit}>
+        <form>
           <div>
-            <label>Title: </label>
-            <br />
+            <label>Title: </label> <br />
             <input
               type="text"
               name="title"
-              onChange={this.onChange}
+            //   onChange={this.onChange}
               value={this.state.title}
             />
           </div>
           <br />
           <div>
-            <label>Body: </label>
-            <br />
+            <label>Body: </label> <br />
             <textarea
-              name="body"
-              onChange={this.onChange}
-              value={this.state.body}
+               name="body"
+            //    onChange={this.onChange}
+               value={this.state.body}
             />
           </div>
           <br />
@@ -63,8 +47,4 @@ class PostForm extends Component {
   }
 }
 
-PostForm.propTypes = {
-  createPost: PropTypes.func.isRequired
-};
-
-export default connect(null, { createPost })(PostForm);
+export default Postform;
