eb4bcc99fb9004a3808f1b04fa4ae8d629dcdcfe
iamomoro <iamomoroit@gmail.com>
2022-03-15 11:15:00 +0300

    posts

--- src/components/posts.js
+++ src/components/posts.js
@@ -1,9 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-class posts extends Component {
+
+class Posts extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      posts: []
+    }
+  }
+
+  componentWillMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res =>res.json())
+    .then(data => this.setState({posts: data}));
+  }
   render() {
-    return (
-      <div>posts</div>
-    )
+    return <div> 
+      <h1>Posts</h1>
+     </div>;
   }
 }
+
+export default Posts;
