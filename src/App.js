99bf193260fef8bd6bced8ac7875ed02c291fd12
iamomoro <iamomoroit@gmail.com>
2022-03-15 11:26:43 +0300

    render-return

--- src/App.js
+++ src/App.js
@@ -5,20 +5,12 @@ import Posts from './components/posts';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
         
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Posts />
+              
       </header>
-
+      <Posts />
      
     </div>
   );
